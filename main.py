7bafc3a926e876159308fef5c878cb6429c1b390
Hatyman <38334017+Hatyman@users.noreply.github.com>
2018-04-22 21:42:21 +0700

    main
    Environment has improved.
    + Plot

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 
 import random
 import numpy as np
-import math
+import matplotlib.pyplot as plt
 
+
+
+ITER = 100
 E = 0.1
 N = 10
-Q = np.zeros([N, 4])
-R = np.zeros([N, 1])
-A = np.zeros([N, 1])
 greedy = 0
 T = 0
 tau = 5
+
+Q = np.zeros([N, 4])
+R = np.zeros([N, 1])
+A = np.zeros([N, 1])
+PLOT = np.zeros([ITER, 1])
+
+
+
+def surround(t, a):
+    for i in range(N):
+        A[i, 0] = random.randint(-1000, 1000) / 1000
+    R[:, 0] += (((t + 1) * (t + 1) - (t * t)) * A[:, 0]) / 2
+    return R[a, 0]
 
 def index():
     global Q
@@ -25,13 +38,18 @@ def index():
 def Quality(a, t):
     global Q, R, T
     T = 0
+    M = 0
     for i in range(N):
         T += np.exp(Q[i, 2] / tau)
-    a -= 1
+    # a -= 1
     Q[a, 0] += 1
-    Q[a, 1] += R[a, 0]
-    Q[a, 2] = Q[a, 1] / Q[a, 0] + (1 / (t + 1) * ((((t + 2) * (t + 2) - (t + 1) * (t + 1)) * A[a, 0]) / 2 - Q[a, 1] / Q[a, 0]))
-    Q[a, 3] = np.exp(Q[a, 2] / tau) / T
+    # Q[a, 1] += R[a, 0]
+    # Q[a, 2] = Q[a, 1] / Q[a, 0]
+    Q[a, 2] = M
+    Q[a, 2] = M + 1/(t + 1) * (surround(t, a) - M)
+    if T != 0:
+        Q[a, 3] = np.exp(Q[a, 2] / tau) / T
+    PLOT[t, 0] = Q[a, 2]
 
 def Nchoice(c):
     S = random.choices(range(N), weights=Q[:, 3])
@@ -40,56 +58,23 @@ def Nchoice(c):
     return S[0]
 
 
-# class Vehicle(object):
-#     """docstring"""
-#
-#     def __init__(self, color, doors, tires, vtype):
-#         """Constructor"""
-#         self.color = color
-#         self.doors = doors
-#         self.tires = tires
-#         self.vtype = vtype
-#
-#     def brake(self):
-#         """
-#         Stop the car
-#         """
-#         return "%s braking" % self.vtype
-#
-#     def drive(self):
-#         """
-#         Drive the car
-#         """
-#         return "I'm driving a %s %s!" % (self.color, self.vtype)
-#
-#
-# if __name__ == "__main__":
-#     car = Vehicle("blue", 5, 4, "car")
-#     print(car.brake())
-#     print(car.drive())
-#
-#     truck = Vehicle("red", 3, 6, "truck")
-#     print(truck.drive())
-#     print(truck.brake())
 
 for i in range(N):
-    Q[i, 2] = random.random()
-    Q[i, 3] = random.random()
-    A[i, 0] = random.randint(-1000, 1000) / 100000
+    Quality(i, 0)
+    # Q[i, 2] = random.random()
+    # Q[i, 3] = random.random()
     T += np.exp(Q[i, 2] / tau)
 
 greedy = index()
 
-for i in range(100):
-    R[:, 0] += (((i + 1) * (i + 1) - (i * i)) * A[:, 0]) / 2
+for i in range(ITER):
     way = random.random()
-    if i < 100:
-        if way <= E:
+    if way <= E:
             act = Nchoice(greedy)
             Quality(act, i)
-        else:
-            Quality(greedy + 1, i)
     else:
-        Quality(greedy + 1, i)
+            Quality(greedy, i)
     greedy = index()
+    plt.scatter(i, PLOT[i])
 print(Q[:, 2], Q[:, 0], greedy, Q[greedy, 2])
+plt.show()
