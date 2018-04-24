5f4e50b9554043a5cf98debf7410c2829b017df0
Hatyman <38334017+Hatyman@users.noreply.github.com>
2018-04-24 13:02:35 +0700

    main
    grafics

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ tau = 5
 Q = np.zeros([N, 4])
 R = np.zeros([N, 1])
 A = np.zeros([N, 1])
-PLOT = np.zeros([ITER, 1])
+PLOT = np.zeros([ITER, 2])
 
 
 
@@ -24,6 +24,7 @@ def surround(t, a):
         A[i, 0] = random.randint(-1000, 1000) / 1000
     R[:, 0] += (((t + 1) * (t + 1) - (t * t)) * A[:, 0]) / 2
     return R[a, 0]
+
 
 def index():
     global Q
@@ -34,6 +35,7 @@ def index():
             max = Q[i, 2]
             pos = i
     return pos
+
 
 def Quality(a, t):
     global Q, R, T
@@ -46,10 +48,12 @@ def Quality(a, t):
     # Q[a, 1] += R[a, 0]
     # Q[a, 2] = Q[a, 1] / Q[a, 0]
     Q[a, 2] = M
-    Q[a, 2] = M + 1/(t + 1) * (surround(t, a) - M)
+    Q[a, 2] = M + 1 / (t + 1) * (surround(t, a) - M)
     if T != 0:
         Q[a, 3] = np.exp(Q[a, 2] / tau) / T
-    PLOT[t, 0] = Q[a, 2]
+    PLOT[t, 0] = surround(t, a) / (t + 1)
+    PLOT[t, 1] = Q[a, 2]
+
 
 def Nchoice(c):
     S = random.choices(range(N), weights=Q[:, 3])
@@ -58,14 +62,15 @@ def Nchoice(c):
     return S[0]
 
 
-
 for i in range(N):
     Quality(i, 0)
     # Q[i, 2] = random.random()
     # Q[i, 3] = random.random()
     T += np.exp(Q[i, 2] / tau)
 
+
 greedy = index()
+
 
 for i in range(ITER):
     way = random.random()
@@ -75,6 +80,7 @@ for i in range(ITER):
     else:
             Quality(greedy, i)
     greedy = index()
-    plt.scatter(i, PLOT[i])
+    plt.scatter(i, PLOT[i, 0], edgecolors='000')
+    plt.scatter(i, PLOT[i, 1])
 print(Q[:, 2], Q[:, 0], greedy, Q[greedy, 2])
 plt.show()
